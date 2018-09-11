4ed131b45caa080fa5cf2510c88495710d5a3635
cclauss <cclauss@bluewin.ch>
2018-09-11 08:07:21 +0200

    Convert async --> non_blocking for Python 3.7

--- tools/launch_tensorboard.py
+++ tools/launch_tensorboard.py
@@ -11,5 +11,5 @@ task = ncluster.make_task('tensorboard',
                           instance_type='r5.large',
                           image_name='Deep Learning AMI (Ubuntu) Version 13.0')
 task.run('source activate tensorflow_p36')
-task.run(f'tensorboard --logdir={task.logdir}/..', async=True)
+task.run(f'tensorboard --logdir={task.logdir}/..', non_blocking=True)
 print(f"Tensorboard at http://{task.public_ip}:6006")
